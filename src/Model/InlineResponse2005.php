e7d24b4ee7c4bfd366a48a732d7a4d38db4a67ec
Gerard van Beijeren <info@bbh-projects.com>
2023-05-25 14:57:28 +0200

    Errors resolved

--- src/Model/InlineResponse2005.php
+++ src/Model/InlineResponse2005.php
@@ -328,7 +328,7 @@ class InlineResponse2005 implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset):bool
     {
         return isset($this->container[$offset]);
     }
